98238aac5582f2c82c6750e39d549ae7144463aa
crashoz <xavier.launey@gmail.com>
2025-11-16 17:07:46 +0100

    fix var shadowing

--- src/usenc/core.py
+++ src/usenc/core.py
@@ -4,7 +4,7 @@ def encode(text: str, encoder_name: str, **encoder_params) -> str:
     """Encode a single text string"""
     encoder = ENCODERS.get(encoder_name)
     if not encoder:
-        raise ValueError(f"Unknown encoder: {encoder}")
+        raise ValueError(f"Unknown encoder: {encoder_name}")
 
     return encoder.encode(text, **encoder_params)
 
@@ -12,7 +12,7 @@ def decode(text: str, encoder_name: str, **encoder_params) -> str:
     """Decode a single text string"""
     encoder = ENCODERS.get(encoder_name)
     if not encoder:
-        raise ValueError(f"Unknown encoder: {encoder}")
+        raise ValueError(f"Unknown encoder: {encoder_name}")
 
     return encoder.decode(text, **encoder_params)
 

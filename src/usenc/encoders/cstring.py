0cb1b60a561597ed0d9ebd827f57e25e7642ddb0
crashoz <xavier.launey@gmail.com>
2025-12-13 19:56:44 +0100

    New encoder

---
+++ src/usenc/encoders/cstring.py
@@ -0,0 +1,26 @@
+from .encoder import Encoder
+from .hex import HexEncoder
+
+class CString(HexEncoder):
+    """
+    C string escaping
+
+    Encodes special characters and utf8 characters with a \\x 
+    prefixed hex value. 
+
+    Examples:
+    hello world -> hello\\x20world
+    url$param+ -> url\\x24param\\x2B
+    """
+
+    prefix = '\\x'
+
+    # Exclude prefix parameter since it's defined as a class attribute
+    params = {k: v for k, v in HexEncoder.params.items() if k not in set(['prefix'])}
+
+    tests = {
+        'base': {
+            'params': '',
+            'roundtrip': True
+        }
+    }
